75c6fad63da6c99e4953ece98bddba73725f417e
heizelnut <upbeatshaw@protonmail.com>
2021-06-25 23:39:47 +0200

    📷 Screen REC module

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*/   /*Command*/		/*Update Interval*/	/*Update Signal*/	
+	{"", "isrecording",		2,	0},
 	{"\uf1eb ", "wireless",		2,	0},
 	{"\uf0ac ", "active",		5,	0},
 	{"\uf187 ", "newpackages",	5,	0},
@@ -9,7 +10,7 @@ static const Block blocks[] = {
 	{"\uf2db ", "ram",		3,	0},
 	{"\uf0e4 ", "cpu",		5,	0},
 	{"", "battery",			5,	0},
-	{"\uf017  ","datetime",		60,	0},
+	{"\uf017  ","datetime",		15,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
