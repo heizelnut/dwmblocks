991421b665ad378c798ec55a4c575209a5fea08c
heizelnut <upbeatshaw@protonmail.com>
2022-04-21 09:00:54 +0200

    Remove icons and use ASCII text

--- blocks.h
+++ blocks.h
@@ -1,17 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*/   /*Command*/		/*Update Interval*/	/*Update Signal*/	
-	{"", "isrecording",		2,	0},
-	{"\uf1eb ", "wireless",		2,	0},
-	{"\uf0ac ", "active",		5,	0},
-	{"\uf187 ", "newpackages",	5,	0},
-	{"\uf0eb ", "brightness",	0,	11},
-	{"\uf028 ", "volume",		0,	10},
-	{"\uf2db ", "ram",		3,	0},
-	{"\uf0e4 ", "cpu",		5,	0},
-	{"", "battery",			5,	0},
-	{"\uf017  ","datetime",		15,	0},
+	{"", "sb recording",			2,	0},
+	{"ip: ", "sb wireless",		2,	0},
+	{"new: ", "sb packages",	5,	0},
+	{"vol: ", "sb volume",			2,	10},
+	{"bat: ", "sb battery",		5,	0},
+	{"mem: ", "sb ram",			3,	0},
+	{"cpu: ", "sb cpu",			5,	0},
+	{"","sb datetime",				15,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = "    ";
+static char *delim = " | ";
